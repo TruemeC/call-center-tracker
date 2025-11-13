d72b61af376553710296351ec13ef456a954dde9
TruemeC <marketingtrueme@gmail.com>
2025-11-13 20:38:23 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// تم تحديث المسار الأساسي ليطابق اسم المستودع call-center-tracker/
 export default defineConfig({
   plugins: [react()],
+  base: '/call-center-tracker/', 
 });
